a7e6790e07f9d39bd1662eeef271ad7c067efe7a
artechspirit <artechspirit@gmail.com>
2025-05-29 22:11:03 +0700

    Refactor Navbar to conditionally render user auth components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,17 +16,19 @@ const Navbar = async () => {
         </div>
       </Link>
 
-      <div className="flex items-center gap-8">
-        {userId && <NavItems />}
-        <SignedOut>
-          <SignInButton>
-            <button className="btn-signin">Sign In</button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div>
+      {userId && (
+        <div className="flex items-center gap-8">
+          <NavItems />
+          <SignedOut>
+            <SignInButton>
+              <button className="btn-signin">Sign In</button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      )}
     </nav>
   );
 };
